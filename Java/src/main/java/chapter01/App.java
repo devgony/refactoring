c465b881a8757766e611b665c019dcbd1a726d2e
devgony <devgony@gmail.com>
2025-01-16 11:02:28 +0900

    Inline Variable: remove thisAmount

--- Java/src/main/java/chapter01/App.java
+++ Java/src/main/java/chapter01/App.java
@@ -45,8 +45,6 @@ public class App {
         String result = "Statement for " + invoice.customer + "\n";
 
         for (Performance perf : invoice.performances) {
-            int thisAmount = amountFor(perf);
-
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
@@ -54,8 +52,8 @@ public class App {
                 volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += playFor(perf).name + ": " + format(thisAmount / 100) + " (" + perf.audience + " seats)\n";
-            totalAmount += thisAmount;
+            result += playFor(perf).name + ": " + format(amountFor(perf) / 100) + " (" + perf.audience + " seats)\n";
+            totalAmount += amountFor(perf);
         }
         result += "Amount owed is " + format(totalAmount / 100) + "\n";
         result += "You earned " + volumeCredits + " credits\n";
