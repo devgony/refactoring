d54642c0da590a316c177c9cd5a7bbe915fa3838
devgony <devgony@gmail.com>
2025-01-16 18:36:37 +0900

    Rename: format to usd

--- Java/src/main/java/chapter01/App.java
+++ Java/src/main/java/chapter01/App.java
@@ -31,7 +31,7 @@ public class App {
         String type;
     }
 
-    String format(int number) {
+    String usd(int number) {
         NumberFormat formatter = NumberFormat.getInstance(Locale.US);
         formatter.setMinimumFractionDigits(2);
 
@@ -49,10 +49,10 @@ public class App {
             volumeCredits += volumeCreditsFor(perf);
 
             // print line for this order
-            result += playFor(perf).name + ": " + format(amountFor(perf) / 100) + " (" + perf.audience + " seats)\n";
+            result += playFor(perf).name + ": " + usd(amountFor(perf) / 100) + " (" + perf.audience + " seats)\n";
             totalAmount += amountFor(perf);
         }
-        result += "Amount owed is " + format(totalAmount / 100) + "\n";
+        result += "Amount owed is " + usd(totalAmount / 100) + "\n";
         result += "You earned " + volumeCredits + " credits\n";
 
         return result;
