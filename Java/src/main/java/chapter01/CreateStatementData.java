6ab9216c5147a6d8c366d70f3020193025669f61
devgony <devgony@gmail.com>
2025-01-17 16:29:21 +0900

    Add htmlStatement, renderHtml, htmlStatement

--- Java/src/main/java/chapter01/CreateStatementData.java
+++ Java/src/main/java/chapter01/CreateStatementData.java
@@ -3,7 +3,6 @@ package chapter01;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- Java/src/main/java/chapter01/Statement.java
+++ Java/src/main/java/chapter01/Statement.java
@@ -33,4 +33,24 @@ public class Statement {
 
         return "$" + formatter.format(number);
     }
+
+    String htmlStatement(Invoice invoice, Map<String, Play> plays) {
+        CreateStatementData csd = new CreateStatementData(plays);
+        return renderHtml(csd.createStatementData(invoice, plays));
+    }
+
+    String renderHtml(StatementData data) {
+        String result = "<h1>Statement for ${data.customer}</h1>\n";
+        result += "<table>\n";
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+        for (Performance perf : data.performances) {
+            result += "  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>";
+            result += "<td>${usd(perf.amount)}</td></tr>\n";
+        }
+        result += "</table>\n";
+        result += "<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n";
+        result += "<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n";
+
+        return result;
+    }
 }
