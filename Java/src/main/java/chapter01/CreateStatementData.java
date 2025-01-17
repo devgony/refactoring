31008cb1aa05738b825d0f5c944349d542e28128
devgony <devgony@gmail.com>
2025-01-17 16:19:14 +0900

    Separate into Two Files

---
+++ Java/src/main/java/chapter01/CreateStatementData.java
@@ -0,0 +1,110 @@
+package chapter01;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+class CreateStatementData {
+    Map<String, Play> plays;
+
+    @Data
+    static class Invoice {
+        String customer;
+        List<Performance> performances;
+    }
+
+    @Data
+    @NoArgsConstructor
+    static class Performance {
+        public Performance(String playID, int audience) {
+            this.playID = playID;
+            this.audience = audience;
+        }
+
+        String playID;
+        int audience;
+        Play play;
+        int amount;
+        int volumeCredits;
+    }
+
+    @Data
+    static class Play {
+        String name;
+        String type;
+    }
+
+    @Data
+    static class StatementData {
+        String customer;
+        List<Performance> performances;
+        int totalAmount;
+        int totalVolumeCredits;
+    }
+
+    public StatementData createStatementData(Invoice invoice, Map<String, Play> plays) throws Error {
+        StatementData data = new StatementData();
+        data.customer = invoice.customer;
+        data.performances = invoice.performances.stream().map(this::enrichPerformance).collect(Collectors.toList());
+        data.totalAmount = totalAmount(data);
+        data.totalVolumeCredits = totalVolumeCredits(data);
+
+        return data;
+    }
+
+    private Performance enrichPerformance(Performance aPerformance) {
+        Performance result = new Performance(aPerformance.playID, aPerformance.audience);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+
+        return result;
+    };
+
+    private int totalAmount(StatementData data) throws Error {
+        return data.performances.stream().mapToInt(Performance::getAmount).sum();
+    }
+
+    private int totalVolumeCredits(StatementData data) {
+        return data.performances.stream().mapToInt(Performance::getVolumeCredits).sum();
+    }
+
+    private int volumeCreditsFor(Performance aPerformance) {
+        int result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy".equals(aPerformance.play.type))
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    private Play playFor(Performance aPerformance) {
+        return plays.get(aPerformance.playID);
+    }
+
+    private int amountFor(Performance aPerformance) throws Error {
+        int result = 0;
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error("unknown type: " + aPerformance.play.type);
+        }
+        return result;
+    }
+}
--- Java/src/main/java/chapter01/Statement.java
+++ Java/src/main/java/chapter01/Statement.java
@@ -1,82 +1,25 @@
 package chapter01;
 
 import java.text.NumberFormat;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
+import chapter01.CreateStatementData.Invoice;
+import chapter01.CreateStatementData.Performance;
+import chapter01.CreateStatementData.Play;
+import chapter01.CreateStatementData.StatementData;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
 public class Statement {
-    Map<String, Play> plays;
+    String statement(Invoice invoice, Map<String, Play> plays) {
+        CreateStatementData csd = new CreateStatementData(plays);
 
-    @Data
-    static class Invoice {
-        String customer;
-        List<Performance> performances;
+        return renderPlainText(csd.createStatementData(invoice, plays));
     }
-
-    @Data
-    @NoArgsConstructor
-    static class Performance {
-        public Performance(String playID, int audience) {
-            this.playID = playID;
-            this.audience = audience;
-        }
-
-        String playID;
-        int audience;
-        Play play;
-        int amount;
-        int volumeCredits;
-    }
-
-    @Data
-    static class Play {
-        String name;
-        String type;
-    }
-
-    @Data
-    static class StatementData {
-        String customer;
-        List<Performance> performances;
-        int totalAmount;
-        int totalVolumeCredits;
-    }
-
-    String statement(Invoice invoice) {
-        return renderPlainText(createStatementData(invoice, this.plays));
-    }
-
-    private StatementData createStatementData(Invoice invoice, Map<String, Play> plays) throws Error {
-        StatementData data = new StatementData();
-        data.customer = invoice.customer;
-        data.performances = invoice.performances.stream().map(this::enrichPerformance).collect(Collectors.toList());
-        data.totalAmount = totalAmount(data);
-        data.totalVolumeCredits = totalVolumeCredits(data);
-
-        return data;
-    }
-
-    private Performance enrichPerformance(Performance aPerformance) {
-        Performance result = new Performance(aPerformance.playID, aPerformance.audience);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-
-        return result;
-    };
 
     private String renderPlainText(StatementData data) throws Error {
         String result = "Statement for " + data.customer + "\n";
 
         for (Performance perf : data.performances) {
-            result += perf.play.name + ": " + usd(amountFor(perf) / 100) + " (" + perf.audience + " seats)\n";
+            result += perf.play.name + ": " + usd(perf.amount / 100) + " (" + perf.audience + " seats)\n";
         }
         result += "Amount owed is " + usd(data.totalAmount / 100) + "\n";
         result += "You earned " + data.totalVolumeCredits + " credits\n";
@@ -89,47 +32,5 @@ public class Statement {
         formatter.setMinimumFractionDigits(2);
 
         return "$" + formatter.format(number);
-    }
-
-    private int totalAmount(StatementData data) throws Error {
-        return data.performances.stream().mapToInt(Performance::getAmount).sum();
-    }
-
-    private int totalVolumeCredits(StatementData data) {
-        return data.performances.stream().mapToInt(Performance::getVolumeCredits).sum();
-    }
-
-    private int volumeCreditsFor(Performance aPerformance) {
-        int result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy".equals(aPerformance.play.type))
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    private Play playFor(Performance aPerformance) {
-        return plays.get(aPerformance.playID);
-    }
-
-    private int amountFor(Performance aPerformance) throws Error {
-        int result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error("unknown type: " + aPerformance.play.type);
-        }
-        return result;
     }
 }
