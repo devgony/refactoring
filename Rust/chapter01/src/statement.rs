343dae4fd0e6096f7a008a2876b9e65d2002bcfe
devgony <devgony@gmail.com>
2025-02-03 20:29:35 +0900

    Add html_statement, render_html

--- Rust/chapter01/src/statement.rs
+++ Rust/chapter01/src/statement.rs
@@ -33,6 +33,38 @@ pub fn statement<'a>(invoice: Invoice<'a>, plays: HashMap<&str, Play<'a>>) -> St
     render_plain_text(createStatementData(&invoice, &plays))
 }
 
+pub fn html_statement(invoice: Invoice<'_>, plays: HashMap<&str, Play<'_>>) -> String {
+    return render_html(createStatementData(&invoice, &plays));
+}
+
+fn render_html(data: StatementData) -> String {
+    let mut result = format!("<h1>Statement for {}</h1>\n", data.customer);
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for perf in data.performances {
+        result += format!(
+            "  <tr><td>{}</td><td>{}</td>",
+            perf.play.as_ref().unwrap().name,
+            perf.audience
+        )
+        .as_str();
+        result += format!("<td>{}</td></tr>\n", usd(perf.amount.unwrap() as f64)).as_str();
+    }
+    result += "</table>\n";
+    result += format!(
+        "<p>Amount owed is <em>{}</em></p>\n",
+        usd(data.total_amount.unwrap() as f64)
+    )
+    .as_str();
+    result += format!(
+        "<p>You earned <em>{}</em> credits</p>\n",
+        data.total_volume_credits.unwrap()
+    )
+    .as_str();
+
+    result
+}
+
 fn createStatementData<'a>(
     invoice: &'a Invoice<'a>,
     plays: &'a HashMap<&'a str, Play<'a>>,
--- Rust/chapter01/tests/statement.rs
+++ Rust/chapter01/tests/statement.rs
@@ -1,59 +1,76 @@
-use chapter01::statement::{statement, Invoice, Performance, Play};
-use std::collections::HashMap;
+#[cfg(test)]
+mod tests {
+    use chapter01::statement::{html_statement, statement, Invoice, Performance, Play};
+    use std::collections::HashMap;
 
-#[test]
-fn test_statement() {
-    let invoice = Invoice {
-        performances: vec![
-            Performance {
-                play_id: "hamlet",
-                play: None,
-                audience: 55,
-                amount: None,
-                volume_credits: None,
-            },
-            Performance {
-                play_id: "as-like",
-                play: None,
-                audience: 35,
-                amount: None,
-                volume_credits: None,
-            },
-            Performance {
-                play_id: "othello",
-                play: None,
-                audience: 40,
-                amount: None,
-                volume_credits: None,
-            },
-        ],
-        customer: "BigCo",
-    };
-    let plays = HashMap::from([
-        (
-            "hamlet",
-            Play {
-                name: "Hamlet",
-                _type: "tragedy",
-            },
-        ),
-        (
-            "as-like",
-            Play {
-                name: "As You Like It",
-                _type: "comedy",
-            },
-        ),
-        (
-            "othello",
-            Play {
-                name: "Othello",
-                _type: "tragedy",
-            },
-        ),
-    ]);
+    fn before_each() -> (Invoice<'static>, HashMap<&'static str, Play<'static>>) {
+        let invoice = Invoice {
+            performances: vec![
+                Performance {
+                    play_id: "hamlet",
+                    play: None,
+                    audience: 55,
+                    amount: None,
+                    volume_credits: None,
+                },
+                Performance {
+                    play_id: "as-like",
+                    play: None,
+                    audience: 35,
+                    amount: None,
+                    volume_credits: None,
+                },
+                Performance {
+                    play_id: "othello",
+                    play: None,
+                    audience: 40,
+                    amount: None,
+                    volume_credits: None,
+                },
+            ],
+            customer: "BigCo",
+        };
 
-    let actual = statement(invoice, plays);
-    let expected = "Statement for BigCo\nHamlet: $650.00 (55 seats)\nAs You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n";
-    assert_eq!(actual, expected);
+        let plays = HashMap::from([
+            (
+                "hamlet",
+                Play {
+                    name: "Hamlet",
+                    _type: "tragedy",
+                },
+            ),
+            (
+                "as-like",
+                Play {
+                    name: "As You Like It",
+                    _type: "comedy",
+                },
+            ),
+            (
+                "othello",
+                Play {
+                    name: "Othello",
+                    _type: "tragedy",
+                },
+            ),
+        ]);
+
+        (invoice, plays)
+    }
+
+    #[test]
+    fn test_statement() {
+        let (invoice, plays) = before_each();
+        let actual = statement(invoice, plays);
+        let expected = "Statement for BigCo\nHamlet: $650.00 (55 seats)\nAs You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_html_statement() {
+        let (invoice, plays) = before_each();
+        let actual = html_statement(invoice, plays);
+        let expected = "<h1>Statement for BigCo</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$65,000.00</td></tr>\n  <tr><td>As You Like It</td><td>35</td><td>$58,000.00</td></tr>\n  <tr><td>Othello</td><td>40</td><td>$50,000.00</td></tr>\n</table>\n<p>Amount owed is <em>$173,000.00</em></p>\n<p>You earned <em>47</em> credits</p>\n";
+        assert_eq!(actual, expected);
+    }
 }
