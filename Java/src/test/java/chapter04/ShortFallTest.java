f87f8a0110dd2f48cef55afaf80ae122e07a1743
devgony <devgony@gmail.com>
2025-02-19 15:50:21 +0900

    Implement Java main; Add first Test::provinceShortFall

---
+++ Java/src/test/java/chapter04/ShortFallTest.java
@@ -0,0 +1,36 @@
+package chapter04;
+
+import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShortFallTest {
+    JsonNode sampleProvinceData() {
+        String jsonString = "{"
+                + "\"name\": \"Asia\","
+                + "\"producers\": ["
+                + "{\"name\": \"Byzantium\", \"cost\": 10, \"production\": 9},"
+                + "{\"name\": \"Attalia\", \"cost\": 12, \"production\": 10},"
+                + "{\"name\": \"Sinope\", \"cost\": 10, \"production\": 6}"
+                + "],"
+                + "\"demand\": 30,"
+                + "\"price\": 20"
+                + "}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readTree(jsonString);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void provinceShortFall() {
+        Province asia = new Province(sampleProvinceData());
+        assertThat(asia.shortfall()).isEqualTo(5);
+    }
+}
