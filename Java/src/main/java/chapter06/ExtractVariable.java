3097941bb0d977b76c638492d2821f8c637a05fc
devgony <devgony@gmail.com>
2025-03-02 18:54:23 +0900

    Add With a Class exmaple

--- Java/src/main/java/chapter06/ExtractVariable.java
+++ Java/src/main/java/chapter06/ExtractVariable.java
@@ -1,19 +1,39 @@
 package chapter06;
 
+import lombok.AllArgsConstructor;
+
 class ExtractVariable {
-    static class Order {
+    @AllArgsConstructor
+    static class OrderData {
         double quantity;
         double itemPrice;
+    }
 
-        public Order(double quantity, double itemPrice) {
-            this.quantity = quantity;
-            this.itemPrice = itemPrice;
+    static class Order {
+        OrderData _data;
+
+        public Order(OrderData aRecord) {
+            this._data = aRecord;
+        }
+
+        double quantity() {
+            return this._data.quantity;
+        }
+
+        double itemPrice() {
+            return this._data.itemPrice;
+        }
+
+        double price2() {
+            return (this.quantity() * this.itemPrice() -
+                    Math.max(0, this.quantity() - 500) * this.itemPrice() * 0.05 +
+                    Math.min(this.quantity() * this.itemPrice() * 0.1, 100));
         }
     }
 
-    static double price(Order order) {
-        double basePrice = order.quantity * order.itemPrice;
-        double quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    static double price(OrderData orderData) {
+        double basePrice = orderData.quantity * orderData.itemPrice;
+        double quantityDiscount = Math.max(0, orderData.quantity - 500) * orderData.itemPrice * 0.05;
         double shipping = Math.min(basePrice * 0.1, 100);
         return basePrice -
                 quantityDiscount +
--- Java/src/test/java/chapter06/ExtractVariableTest.java
+++ Java/src/test/java/chapter06/ExtractVariableTest.java
@@ -3,14 +3,23 @@ package chapter06;
 import org.junit.jupiter.api.Test;
 
 import chapter06.ExtractVariable.Order;
+import chapter06.ExtractVariable.OrderData;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ExtractVariableTest {
     @Test
     void testPrice() {
-        Order order = new Order(100, 10);
-        double actual = ExtractVariable.price(order);
+        OrderData data = new OrderData(100, 10);
+        double actual = ExtractVariable.price(data);
 
+        assertThat(actual).isEqualTo(1100);
+    }
+
+    @Test
+    void withAClass() {
+        Order order = new Order(new OrderData(100, 10));
+        double actual = order.price2();
         assertThat(actual).isEqualTo(1100);
     }
 }
