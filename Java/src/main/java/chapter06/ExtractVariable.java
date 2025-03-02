4d115a58136b3524df99ce08219ac248ac828fba
devgony <devgony@gmail.com>
2025-03-02 18:29:15 +0900

    Extract Variable: shipping

--- Java/src/main/java/chapter06/ExtractVariable.java
+++ Java/src/main/java/chapter06/ExtractVariable.java
@@ -14,9 +14,10 @@ class ExtractVariable {
     static double price(Order order) {
         double basePrice = order.quantity * order.itemPrice;
         double quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+        double shipping = Math.min(basePrice * 0.1, 100);
         return basePrice -
                 quantityDiscount +
-                Math.min(basePrice * 0.1, 100);
+                shipping;
 
     }
 }
