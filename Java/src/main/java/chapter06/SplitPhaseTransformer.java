9ab88900d7da86993f4e85da74f16a95207478ab
devgony <devgony@gmail.com>
2025-03-03 12:22:51 +0900

    Replace Temp with Query: filename

--- Java/src/main/java/chapter06/SplitPhaseTransformer.java
+++ Java/src/main/java/chapter06/SplitPhaseTransformer.java
@@ -18,8 +18,12 @@ class SplitPhaseTransformer {
         if (args.length == 0)
             throw new RuntimeException("must supply a filename");
         CommandLine commandLine = new CommandLine(args);
+        return countOrders(commandLine, args, filename(args));
+    }
+
+    private static String filename(String[] args) {
         String filename = args[args.length - 1];
-        return countOrders(commandLine, args, filename);
+        return filename;
     }
 
     private static long countOrders(CommandLine commandLine, String[] args, String filename) throws IOException {
