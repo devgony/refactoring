45bd806c773e4d143a5e347495618f20d2465f3c
devgony <devgony@gmail.com>
2025-03-04 09:41:10 +0900

    Add HideDelegate base

---
+++ Java/src/main/java/chapter07/HideDelegate.java
@@ -0,0 +1,51 @@
+package chapter07;
+
+class HideDelegate {
+    static class Person {
+        String _name;
+        Department _department;
+
+        Person(String name) {
+            this._name = name;
+        }
+
+        String name() {
+            return this._name;
+        }
+
+        Department department() {
+            return this._department;
+        }
+
+        void department(Department arg) {
+            this._department = arg;
+        }
+    }
+
+    static class Department {
+        private String _chargeCode;
+        private Person _manager;
+
+        Department(String chargeCode, Person manager) {
+            this._chargeCode = chargeCode;
+            this._manager = manager;
+        }
+
+        String chargeCode() {
+            return this._chargeCode;
+        }
+
+        void chargeCode(String arg) {
+            this._chargeCode = arg;
+        }
+
+        Person manager() {
+            return this._manager;
+        }
+
+        void manager(Person arg) {
+            this._manager = arg;
+        }
+    }
+
+}
