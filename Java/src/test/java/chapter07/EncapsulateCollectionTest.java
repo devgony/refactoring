82cc4979f6b2f43d28661873bb2d6e4e3dc74152
devgony <devgony@gmail.com>
2025-03-04 12:15:58 +0900

    Add a case for shouldNotMutateOriginalCollection

--- Java/src/test/java/chapter07/EncapsulateCollectionTest.java
+++ Java/src/test/java/chapter07/EncapsulateCollectionTest.java
@@ -15,4 +15,18 @@ class EncapsulateCollectionTest {
         long numberAdvancedCourses = aPerson.courses().stream().filter(Course::isAdvanced).count();
         assertThat(numberAdvancedCourses).isEqualTo(1);
     }
+
+    @Test
+    void shouldNotMutateOriginalCollection() {
+        Person aPerson = new Person("John Doe");
+        String filename = "courses.txt";
+        for (String name : readBasicCourseNames(filename)) {
+            aPerson.courses().add(new Course(name, false));
+        }
+        assertThat(aPerson.courses().size()).isEqualTo(0);
+    }
+
+    private String[] readBasicCourseNames(String filename) {
+        return new String[] { "Math", "Science" };
+    }
 }
