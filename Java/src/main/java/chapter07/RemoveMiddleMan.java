69d63c8055b86cd8240d066bf069b5f06d3a6cda
devgony <devgony@gmail.com>
2025-03-04 09:47:54 +0900

    Add RemoveMiddleMan base

---
+++ Java/src/main/java/chapter07/RemoveMiddleMan.java
@@ -0,0 +1,54 @@
+package chapter07;
+
+class RemoveMiddleMan {
+    static class Person {
+        String _name;
+        Department _department;
+
+        Person(String name) {
+            this._name = name;
+        }
+
+        String name() {
+            return this._name;
+        }
+
+        Department department() {
+            return this._department;
+        }
+
+        void department(Department arg) {
+            this._department = arg;
+        }
+
+        Person manager() {
+            return this._department.manager();
+        }
+    }
+
+    static class Department {
+        private String _chargeCode;
+        private Person _manager;
+
+        Department(String chargeCode, Person manager) {
+            this._chargeCode = chargeCode;
+            this._manager = manager;
+        }
+
+        String chargeCode() {
+            return this._chargeCode;
+        }
+
+        void chargeCode(String arg) {
+            this._chargeCode = arg;
+        }
+
+        Person manager() {
+            return this._manager;
+        }
+
+        void manager(Person arg) {
+            this._manager = arg;
+        }
+    }
+}
---
+++ Java/src/test/java/chapter07/RemoveMiddleManTest.java
@@ -0,0 +1,19 @@
+package chapter07;
+
+import org.junit.jupiter.api.Test;
+
+import chapter07.RemoveMiddleMan.Department;
+import chapter07.RemoveMiddleMan.Person;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RemoveMiddleManTest {
+    @Test
+    void client() {
+        Person aPerson = new Person("John");
+        Person manager = new Person("Man");
+        Department department = new Department("charcode", manager);
+        aPerson.department(department);
+        Person actual = aPerson.manager();
+        assertThat(actual).isEqualTo(manager);
+    }
+}
