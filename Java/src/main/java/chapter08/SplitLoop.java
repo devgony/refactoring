ae65aaa0f0f08b6e565f7d12e3506045278f4c91
devgony <devgony@gmail.com>
2025-03-13 01:22:31 +0900

    Extract Function: totalSalary()

--- Java/src/main/java/chapter08/SplitLoop.java
+++ Java/src/main/java/chapter08/SplitLoop.java
@@ -12,18 +12,21 @@ class SplitLoop {
     }
 
     static String ageAndSalary(List<Person> people) {
-        int totalSalary = 0;
-        for (Person p : people) {
-            totalSalary += p.salary;
-        }
-
         int youngest = people.isEmpty() ? Integer.MAX_VALUE : people.get(0).age;
         for (Person p : people) {
             if (p.age < youngest)
                 youngest = p.age;
         }
 
-        return "youngestAge: " + youngest + ", totalSalary: " + totalSalary;
+        return "youngestAge: " + youngest + ", totalSalary: " + totalSalary(people);
+    }
+
+    private static int totalSalary(List<Person> people) {
+        int totalSalary = 0;
+        for (Person p : people) {
+            totalSalary += p.salary;
+        }
+        return totalSalary;
     }
 
 }
