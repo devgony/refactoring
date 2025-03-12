2dd02a69c8c2dfb5cced96ed444b26a32e949c13
devgony <devgony@gmail.com>
2025-03-13 01:39:50 +0900

    Move split into map()

--- Java/src/main/java/chapter08/ReplaceLoopWithPipeline.java
+++ Java/src/main/java/chapter08/ReplaceLoopWithPipeline.java
@@ -21,12 +21,13 @@ class ReplaceLoopWithPipeline {
     static List<CityPhoneData> acquireData(String input) {
         String[] lines = input.split("\n");
         List<CityPhoneData> result = new ArrayList<>();
-        List<String> loopItems = Arrays.asList(lines).stream()
+        List<String[]> loopItems = Arrays.asList(lines).stream()
                 .skip(1)
                 .filter(line -> !line.trim().isEmpty())
+                .map(l -> l.split(","))
                 .collect(Collectors.toList());
-        for (String line : loopItems) {
-            String[] record = line.split(",");
+        for (String[] line : loopItems) {
+            String[] record = line;
             if (record[1].trim().equals("India")) {
                 result.add(new CityPhoneData(record[0].trim(), record[2].trim()));
             }
