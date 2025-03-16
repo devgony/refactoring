8d1984f07a5a0b05131c3ff778e56de3ebf3e371
devgony <devgony@gmail.com>
2025-03-16 15:40:24 +0900

    Make variable final

--- Java/src/main/java/chapter09/SplitVariable.java
+++ Java/src/main/java/chapter09/SplitVariable.java
@@ -13,15 +13,15 @@ class SplitVariable {
 
     static double distanceTravelled(Scenario scenario, int time) {
         double result;
-        double primaryAcceleration = scenario.primaryForce / scenario.mass;
+        final double primaryAcceleration = scenario.primaryForce / scenario.mass;
         double primaryTime = Math.min(time, scenario.delay);
         result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
         double secondaryTime = time - scenario.delay;
         if (secondaryTime > 0) {
             double primaryVelocity = primaryAcceleration * scenario.delay;
-            primaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+            final double acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
             result += primaryVelocity * secondaryTime +
-                    0.5 * primaryAcceleration * secondaryTime * secondaryTime;
+                    0.5 * acc * secondaryTime * secondaryTime;
         }
         return result;
     }
