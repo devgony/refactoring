37bec5b9e879d75584f22ec7c84f47bac1225b5f
devgony <devgony@gmail.com>
2025-03-16 15:38:00 +0900

    Rename Variable: acc to priamryAcceleration

--- Java/src/main/java/chapter09/SplitVariable.java
+++ Java/src/main/java/chapter09/SplitVariable.java
@@ -13,15 +13,15 @@ class SplitVariable {
 
     static double distanceTravelled(Scenario scenario, int time) {
         double result;
-        double acc = scenario.primaryForce / scenario.mass;
+        double primaryAcceleration = scenario.primaryForce / scenario.mass;
         double primaryTime = Math.min(time, scenario.delay);
-        result = 0.5 * acc * primaryTime * primaryTime;
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
         double secondaryTime = time - scenario.delay;
         if (secondaryTime > 0) {
-            double primaryVelocity = acc * scenario.delay;
-            acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+            double primaryVelocity = primaryAcceleration * scenario.delay;
+            primaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
             result += primaryVelocity * secondaryTime +
-                    0.5 * acc * secondaryTime * secondaryTime;
+                    0.5 * primaryAcceleration * secondaryTime * secondaryTime;
         }
         return result;
     }
