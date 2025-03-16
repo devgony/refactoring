47d7747681ec56e9f27145ad46498244c5ae07a8
devgony <devgony@gmail.com>
2025-03-16 17:24:02 +0900

    Encapsulate Record: Organization

--- Java/src/main/java/chapter09/RenameField.java
+++ Java/src/main/java/chapter09/RenameField.java
@@ -1,5 +1,32 @@
 package chapter09;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 class RenameField {
+    static class Organization {
+        String _name;
+        String _country;
+
+        Organization(ObjectNode data) {
+            this._name = data.get("name").asText();
+            this._country = data.get("country").asText();
+        }
+
+        String name() {
+            return this._name;
+        }
+
+        void name(String aString) {
+            this._name = aString;
+        }
+
+        String country() {
+            return this._country;
+        }
+
+        void country(String aCountryCode) {
+            this._country = aCountryCode;
+        }
+    }
 
 }
