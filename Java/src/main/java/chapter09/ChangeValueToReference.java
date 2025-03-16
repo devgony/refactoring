e609413dc66fe82243de21a01ca83d01998141d5
devgony <devgony@gmail.com>
2025-03-16 21:41:08 +0900

    use registerCustomer in Order constructor; use static global initialize for repository

--- Java/src/main/java/chapter09/ChangeValueToReference.java
+++ Java/src/main/java/chapter09/ChangeValueToReference.java
@@ -2,28 +2,30 @@ package chapter09;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
 class ChangeValueToReference {
-    Map<String, Map<Integer, Customer>> _repositoryData;
-
-    void initialize() {
-        _repositoryData = new HashMap<>();
-        this._repositoryData.put("customers", new HashMap<>());
+    static Map<String, Map<Integer, Customer>> _repositoryData;
+    static {
+        initialize();
     }
 
-    Customer registerCustomer(int id) {
-        if (!this._repositoryData.get("customers").containsKey(id)) {
-            this._repositoryData.get("customers").put(id, new Customer(id));
+    static void initialize() {
+        _repositoryData = new HashMap<>();
+        _repositoryData.put("customers", new HashMap<>());
+    }
+
+    static Customer registerCustomer(int id) {
+        if (!_repositoryData.get("customers").containsKey(id)) {
+            _repositoryData.get("customers").put(id, new Customer(id));
         }
 
         return findCustomer(id);
     }
 
-    Customer findCustomer(int id) {
-        return this._repositoryData.get("customers").get(id);
+    static Customer findCustomer(int id) {
+        return _repositoryData.get("customers").get(id);
     }
 
     @AllArgsConstructor
@@ -38,7 +40,7 @@ class ChangeValueToReference {
 
         Order(Data data) {
             this._number = data.number;
-            this._customer = new Customer(data.customerId);
+            this._customer = registerCustomer(data.customerId);
             // load other data
         }
 
