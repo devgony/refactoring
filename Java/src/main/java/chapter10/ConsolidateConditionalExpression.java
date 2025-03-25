e4c96479cabf8c75cc1ed84467ceaa42ff3c276a
devgony <devgony@gmail.com>
2025-03-25 19:27:24 +0900

    Consolidate Top 2 conditions

--- Java/src/main/java/chapter10/ConsolidateConditionalExpression.java
+++ Java/src/main/java/chapter10/ConsolidateConditionalExpression.java
@@ -12,9 +12,7 @@ class ConsolidateConditionalExpression {
     }
 
     static int disabilityAmount(Employee anEmployee) {
-        if (anEmployee.seniority < 2)
-            return 0;
-        if (anEmployee.monthsDisabled > 12)
+        if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12)
             return 0;
         if (anEmployee.isPartTime)
             return 0;
