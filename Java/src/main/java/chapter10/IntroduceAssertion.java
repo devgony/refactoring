700b0e54044b127f69103f3451a6ba171c1da157
devgony <devgony@gmail.com>
2025-03-27 15:18:57 +0900

    Move assertion into setter

--- Java/src/main/java/chapter10/IntroduceAssertion.java
+++ Java/src/main/java/chapter10/IntroduceAssertion.java
@@ -4,11 +4,15 @@ class IntroduceAssertion {
     static class Customer {
         Double discountRate;
 
+        void setDiscountRate(Double discountRate) {
+            assert (discountRate == null || discountRate >= 0);
+            this.discountRate = discountRate;
+        }
+
         double applyDiscount(double aNumber) {
             if (discountRate == null)
                 return aNumber;
             else {
-                assert discountRate >= 0;
                 return aNumber - (this.discountRate * aNumber);
             }
         }
