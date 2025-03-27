9638ae16eec8bcf5c37f94a2606d56d4360f7946
devgony <devgony@gmail.com>
2025-03-27 13:10:26 +0900

    Site.customer() uses createUnknownCustomer

--- Java/src/main/java/chapter10/IntroduceSpecialCaseUsingAnObjectLiteral.java
+++ Java/src/main/java/chapter10/IntroduceSpecialCaseUsingAnObjectLiteral.java
@@ -8,39 +8,11 @@ import utils.ObjectBuilder;
 class IntroduceSpecialCaseUsingAnObjectLiteral {
     @AllArgsConstructor
     static class Site {
-        Customer _customer;
+        ObjectNode _customer;
 
-        Customer customer() {
-            return this._customer;
+        ObjectNode customer() {
+            return this._customer.get("name").asText().equals("unknown") ? createUnknownCustomer() : _customer;
         }
-    }
-
-    @AllArgsConstructor
-    static class Customer {
-        String _name;
-        String _billingPlan;
-        PaymentHistory _paymentHistory;
-
-        String name() {
-            return _name;
-        }
-
-        void name(String arg) {
-            _name = arg;
-        }
-
-        String billingPlan() {
-            return _billingPlan;
-        }
-
-        void billingPlan(String arg) {
-            _billingPlan = arg;
-        }
-
-        PaymentHistory paymentHistory() {
-            return _paymentHistory;
-        }
-
     }
 
     @AllArgsConstructor
@@ -66,35 +38,35 @@ class IntroduceSpecialCaseUsingAnObjectLiteral {
         String basic;
     }
 
-    ObjectNode createUnknownCustomer() {
+    static ObjectNode createUnknownCustomer() {
         return ObjectBuilder.readValue("{\"isUnknown\": true}");
     }
 
     static boolean isUnknown(String arg) {
-        return (arg == "unknown");
+        return ("unknown".equals(arg));
     }
 
-    static String client1(Customer aCustomer) {
+    static String client1(ObjectNode aCustomer) {
         String customerName;
-        if (isUnknown(aCustomer.name()))
+        if (isUnknown(aCustomer.get("name").asText()))
             customerName = "occupant";
         else
-            customerName = aCustomer.name();
+            customerName = aCustomer.get("name").asText();
 
         return customerName;
     }
 
-    static String client2(Customer aCustomer) {
+    static String client2(ObjectNode aCustomer) {
         Registery registry = new Registery(new BillingPlans("basic"));
-        String plan = (isUnknown(aCustomer.name())) ? registry.billingPlans().basic
-                : aCustomer.billingPlan();
+        String plan = (isUnknown(aCustomer.get("name").asText())) ? registry.billingPlans().basic
+                : aCustomer.get("billingPlan").asText();
 
         return plan;
     }
 
-    static int client3(Customer aCustomer) {
-        int weeksDelinquent = isUnknown(aCustomer.name()) ? 0
-                : aCustomer.paymentHistory().weeksDelinquentInLastYear();
+    static int client3(ObjectNode aCustomer) {
+        int weeksDelinquent = isUnknown(aCustomer.get("name").asText()) ? 0
+                : aCustomer.get("paymentHistory").get("weeksDelinquentInLastYear").asInt();
 
         return weeksDelinquent;
     }
--- Java/src/test/java/chapter10/IntroduceSpecialCaseUsingAnObjectLiteralTest.java
+++ Java/src/test/java/chapter10/IntroduceSpecialCaseUsingAnObjectLiteralTest.java
@@ -3,40 +3,43 @@ package chapter10;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import chapter10.IntroduceSpecialCaseUsingAnObjectLiteral.Customer;
-import chapter10.IntroduceSpecialCaseUsingAnObjectLiteral.PaymentHistory;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import chapter10.IntroduceSpecialCaseUsingAnObjectLiteral.Site;
+import utils.ObjectBuilder;
+
 import static chapter10.IntroduceSpecialCaseUsingAnObjectLiteral.*;
 
 class IntroduceSpecialCaseUsingAnObjectLiterlTest {
     Site site;
-    Customer aCustomer;
+    ObjectNode record;
+    ObjectNode aCustomer;
+    ObjectNode unknownCustomer;
 
     @BeforeEach
     void setUp() {
-        PaymentHistory paymentHistory = new PaymentHistory(10);
-        site = new Site(new Customer("unknown", "originalBillingPlan", paymentHistory));
+        record = ObjectBuilder.readValue(
+                "{\"name\": \"x\", \"billingPlan\": \"realPlan\", \"paymentHistory\": {\"weeksDelinquentInLastYear\": 10}}");
+        site = new Site(record);
         aCustomer = site.customer();
+        unknownCustomer = ObjectBuilder.readValue("{\"name\": \"unknown\"}");
     }
 
     @Test
     void client1Test() {
-        assertThat(client1(aCustomer)).isEqualTo("occupant");
-        aCustomer.name("x");
+        assertThat(client1(unknownCustomer)).isEqualTo("occupant");
         assertThat(client1(aCustomer)).isEqualTo("x");
     }
 
     @Test
     void client2Test() {
-        assertThat(client2(aCustomer)).isEqualTo("basic");
-        aCustomer.name("x");
-        assertThat(client2(aCustomer)).isEqualTo("originalBillingPlan");
+        assertThat(client2(unknownCustomer)).isEqualTo("basic");
+        assertThat(client2(aCustomer)).isEqualTo("realPlan");
     }
 
     @Test
     void client3Test() {
-        assertThat(client3(aCustomer)).isEqualTo(0);
-        aCustomer.name("x");
+        assertThat(client3(unknownCustomer)).isEqualTo(0);
         assertThat(client3(aCustomer)).isEqualTo(10);
     }
 }
