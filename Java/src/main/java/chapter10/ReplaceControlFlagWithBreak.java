a0c38944a1d1423a127b0955178f57c32dcc65ae
devgony <devgony@gmail.com>
2025-03-27 15:41:59 +0900

    Replace with functional method

--- Java/src/main/java/chapter10/ReplaceControlFlagWithBreak.java
+++ Java/src/main/java/chapter10/ReplaceControlFlagWithBreak.java
@@ -8,15 +8,18 @@ class ReplaceControlFlagWithBreak {
     }
 
     private void checkForMiscreants(List<String> people) {
-        for (String p : people) {
-            if ("Don".equals(p)) {
-                sendAlert();
-                return;
-            }
-            if ("John".equals(p)) {
-                sendAlert();
-                return;
-            }
+        // for (String p : people) {
+        // if ("Don".equals(p)) {
+        // sendAlert();
+        // return;
+        // }
+        // if ("John".equals(p)) {
+        // sendAlert();
+        // return;
+        // }
+        // }
+        if (people.stream().anyMatch(p -> "Don".equals(p) || "John".equals(p))) {
+            sendAlert();
         }
     }
 
