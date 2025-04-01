cabff3a34f04399344e3593d8703c930c36b9b88
devgony <devgony@gmail.com>
2025-04-02 00:33:28 +0900

    Call function directly: {rush, regular}DeliveryDate

--- Java/src/test/java/chapter11/RemoveFlagArgumentTest.java
+++ Java/src/test/java/chapter11/RemoveFlagArgumentTest.java
@@ -3,17 +3,18 @@ package chapter11;
 import org.junit.jupiter.api.Test;
 import chapter11.RemoveFlagArgument.Order;
 import chapter11.RemoveFlagArgument.PlaceOn;
-import static chapter11.RemoveFlagArgument.deliveryDate;
+import static chapter11.RemoveFlagArgument.regularDeliveryDate;
+import static chapter11.RemoveFlagArgument.rushDeliveryDate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RemoveFlagArgumentTest {
     @Test
     void client1() {
         Order anOrder1 = new Order("MA", new PlaceOn());
-        int date1 = deliveryDate(anOrder1, true);
+        int date1 = rushDeliveryDate(anOrder1);
         assertThat(date1).isEqualTo(2);
         Order anOrder2 = new Order("NH", new PlaceOn());
-        int date2 = deliveryDate(anOrder2, false);
+        int date2 = regularDeliveryDate(anOrder2);
         assertThat(date2).isEqualTo(5);
     }
 }
