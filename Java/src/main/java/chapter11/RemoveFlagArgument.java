26c654e6abea43de9caec0981fba7f2a32604f84
devgony <devgony@gmail.com>
2025-04-02 00:26:34 +0900

    Decompose Conditional: {rush, regular}DeliveryDate

--- Java/src/main/java/chapter11/RemoveFlagArgument.java
+++ Java/src/main/java/chapter11/RemoveFlagArgument.java
@@ -22,23 +22,31 @@ class RemoveFlagArgument {
 
     static int deliveryDate(Order anOrder, boolean isRush) {
         if (isRush) {
-            int deliveryTime;
-            if (Arrays.asList("MA", "CT").contains(anOrder.deliveryState))
-                deliveryTime = 1;
-            else if (Arrays.asList("NY", "NH").contains(anOrder.deliveryState))
-                deliveryTime = 2;
-            else
-                deliveryTime = 3;
-            return anOrder.placedOn.plusDays(1 + deliveryTime);
+            return rushDeliveryDate(anOrder);
         } else {
-            int deliveryTime;
-            if (Arrays.asList("MA", "CT", "NY").contains(anOrder.deliveryState))
-                deliveryTime = 2;
-            else if (Arrays.asList("ME", "NH").contains(anOrder.deliveryState))
-                deliveryTime = 3;
-            else
-                deliveryTime = 4;
-            return anOrder.placedOn.plusDays(2 + deliveryTime);
+            return regularDeliveryDate(anOrder);
         }
+    }
+
+    static int rushDeliveryDate(Order anOrder) {
+        int deliveryTime;
+        if (Arrays.asList("MA", "CT").contains(anOrder.deliveryState))
+            deliveryTime = 1;
+        else if (Arrays.asList("NY", "NH").contains(anOrder.deliveryState))
+            deliveryTime = 2;
+        else
+            deliveryTime = 3;
+        return anOrder.placedOn.plusDays(1 + deliveryTime);
+    }
+
+    static int regularDeliveryDate(Order anOrder) {
+        int deliveryTime;
+        if (Arrays.asList("MA", "CT", "NY").contains(anOrder.deliveryState))
+            deliveryTime = 2;
+        else if (Arrays.asList("ME", "NH").contains(anOrder.deliveryState))
+            deliveryTime = 3;
+        else
+            deliveryTime = 4;
+        return anOrder.placedOn.plusDays(2 + deliveryTime);
     }
 }
