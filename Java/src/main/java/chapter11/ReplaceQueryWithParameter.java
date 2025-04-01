04ca34d654e1ce02e300e4036587281b52bf9f85
devgony <devgony@gmail.com>
2025-04-02 07:39:10 +0900

    Exatract Variable: selectedTemperature

--- Java/src/main/java/chapter11/ReplaceQueryWithParameter.java
+++ Java/src/main/java/chapter11/ReplaceQueryWithParameter.java
@@ -16,12 +16,13 @@ class ReplaceQueryWithParameter {
         int _max;
 
         double targetTemperature() {
-            if (thermostat.selectedTemperature > this._max)
+            double selectedTemperature = thermostat.selectedTemperature;
+            if (selectedTemperature > this._max)
                 return this._max;
-            else if (thermostat.selectedTemperature < this._min)
+            else if (selectedTemperature < this._min)
                 return this._min;
             else
-                return thermostat.selectedTemperature;
+                return selectedTemperature;
         }
 
         String client1() {
