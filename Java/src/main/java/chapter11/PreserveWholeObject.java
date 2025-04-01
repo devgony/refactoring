b5b6dfcf302dedb728d3ddd2ef1a080a8408c5d6
devgony <devgony@gmail.com>
2025-04-01 14:31:21 +0900

    Add PreserveWholeObject base

---
+++ Java/src/main/java/chapter11/PreserveWholeObject.java
@@ -0,0 +1,31 @@
+package chapter11;
+
+import lombok.AllArgsConstructor;
+
+class PreserveWholeObject {
+    @AllArgsConstructor
+    static class Room {
+        Range daysTempRange;
+    }
+
+    @AllArgsConstructor
+    static class Range {
+        int low;
+        int high;
+    }
+
+    @AllArgsConstructor
+    static class HeatingPlan {
+        Range _temperatureRange;
+
+        boolean withinRange(int bottom, int top) {
+            return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
+        }
+
+        boolean withinRange2(int bottom, int top) {
+            return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
+        }
+
+    }
+
+}
---
+++ Java/src/test/java/chapter11/PreserveWholeObjectTest.java
@@ -0,0 +1,40 @@
+package chapter11;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import chapter11.PreserveWholeObject.HeatingPlan;
+import chapter11.PreserveWholeObject.Range;
+import chapter11.PreserveWholeObject.Room;
+
+class PreserveWholeObjectTest {
+    @Test
+    void client1() {
+        Range range = new Range(10, 20);
+        Room aRoom = new Room(range);
+        int low = aRoom.daysTempRange.low;
+        int high = aRoom.daysTempRange.high;
+        HeatingPlan aPlan = new HeatingPlan(range);
+        boolean actual = aPlan.withinRange(low, high);
+        assertTrue(actual);
+
+        actual = aPlan.withinRange(low, high + 1);
+        assertFalse(actual);
+        // if (!aPlan.withinRange(low, high))
+        // System.out.println("room temperature went outside range");
+    }
+
+    @Test
+    void client2() {
+        Range range = new Range(10, 20);
+        Room aRoom = new Room(range);
+        int low = aRoom.daysTempRange.low;
+        int high = aRoom.daysTempRange.high;
+        HeatingPlan aPlan = new HeatingPlan(range);
+        boolean actual = aPlan.withinRange2(low, high);
+        assertTrue(actual);
+
+        actual = aPlan.withinRange2(low, high + 1);
+        assertFalse(actual);
+    }
+}
