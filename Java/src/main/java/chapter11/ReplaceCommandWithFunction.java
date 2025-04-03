025215872f3a593008f304a6f4e8d70fe13ddbd3
devgony <devgony@gmail.com>
2025-04-03 09:16:26 +0900

    Remove Dead Code: ChargeCalculator

--- Java/src/main/java/chapter11/ReplaceCommandWithFunction.java
+++ Java/src/main/java/chapter11/ReplaceCommandWithFunction.java
@@ -25,24 +25,6 @@ class ReplaceCommandWithFunction {
         }
     }
 
-    static class ChargeCalculator {
-        Customer _customer;
-        int _usage;
-        Provider _provider;
-
-        ChargeCalculator(Customer customer, int usage, Provider provider) {
-            this._customer = customer;
-            this._usage = usage;
-            this._provider = provider;
-        }
-
-        double charge(Customer customer, int usage, Provider provider) {
-            double baseCharge = customer.baseRate * usage;
-
-            return baseCharge + provider.connectionCharge;
-        }
-    }
-
     static double charge(Customer customer, int usage, Provider provider) {
         double baseCharge = customer.baseRate * usage;
 
