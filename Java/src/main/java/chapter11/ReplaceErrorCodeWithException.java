242146fc56a6b24e92af1b1eb6291b0beb1d0a36
devgony <devgony@gmail.com>
2025-04-03 15:24:55 +0900

    Inspect with trap code

--- Java/src/main/java/chapter11/ReplaceErrorCodeWithException.java
+++ Java/src/main/java/chapter11/ReplaceErrorCodeWithException.java
@@ -37,6 +37,8 @@ class ReplaceErrorCodeWithException {
         if (shippingRules < 0)
             return shippingRules;
         // more irrelevent code..
+        if (shippingRules < 0)
+            throw new Error("error code not dead yet");
         return 0;
     }
 
